59d58236ed0be14a89ab04b7baf6289580fb5d1b
freezach <117788377+freezach@users.noreply.github.com>
2022-11-27 17:23:08 -0700

    Add files via upload

---
+++ enter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <h1>Type your name to enter the game!</h1>
+</head>
+<body>
+  <form class="" action="index.html" method="post">
+    <textarea name="name" rows="8" cols="80">Enter Name:</textarea>
+    <input type="submit" name="" value="">
+  </form>
+</body>
+
+</html>
---
+++ lobby.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <h1>LOBBY</h1>
+</head>
+<body>
+  <h2>Please wait for everyone to arrive!</h2>
+  <?php if (condition/*if first person to arrive in lobby*/) {
+    // code...
+    echo "  <h2>Congrats! You have been selected to be GameMaster!</h2>
+      <h3>When everyone has arrived,
+        select the button to close the lobby and start the game</h3>
+        <button type='button' name='button'>Close Lobby & Start Game</button>";
+  }
+
+     ?>
+</body>
+
+</html>
